43401417a0cdf83daca0862cd5b613fe12e6ccc4
Admin <Admin@DESKTOP-P14QAEA>
2018-09-28 09:22:54 +0200

    Début TP jusqu'à la doc

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string NomDevise { get; set; }
+
+        public double Taux { get; set; }
+
+        public Devise()
+        {
+
+        }
+        public Devise(int id, string nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+    }
+}
